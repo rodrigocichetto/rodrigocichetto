ba74ae562250f97f95cb67dfd6a093d5f2e32134
rodrigocichetto <rodrigocichetto@gmail.com>
2018-08-06 11:22:35 -0300

    add webpack

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+
+      options: {
+        presets: ['env']
+      }
+    }, {
+      test: /\.(scss|css)$/,
+
+      use: [{
+        loader: MiniCssExtractPlugin.loader
+      }, {
+        loader: 'css-loader',
+
+        options: {
+          sourceMap: true
+        }
+      }, {
+        loader: 'sass-loader',
+
+        options: {
+          sourceMap: true
+        }
+      }]
+    }]
+  },
+
+  plugins: [new UglifyJSPlugin(), new MiniCssExtractPlugin({ filename:'style.css' })],
+  entry: ./src/js/app,
+
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: 'production'
+}
