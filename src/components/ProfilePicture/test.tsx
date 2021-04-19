b1fa423f865bbcc2a980870971b8d1968d7f084a
rodrigocichetto <rodrigocichetto@gmail.com>
2021-04-19 17:54:23 -0300

    feat: add ProfilePicture component

---
+++ src/components/ProfilePicture/test.tsx
@@ -0,0 +1,39 @@
+import theme from 'styles/theme';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import ProfilePicture from '.';
+
+describe('<ProfilePicture />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<ProfilePicture src="/teste" />);
+
+    expect(container.querySelector('img')?.src).toContain('/teste');
+    expect(container.querySelector('span')).toHaveStyle({
+      backgroundColor: theme.colors.menu
+    });
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render with different color', () => {
+    const { container } = renderWithTheme(
+      <ProfilePicture src="/teste" color="primary" />
+    );
+
+    expect(container.querySelector(`span`)).toHaveStyle({
+      backgroundColor: theme.colors.primary
+    });
+  });
+
+  it('should render with different background', () => {
+    const { container } = renderWithTheme(
+      <ProfilePicture src="/teste" background="/teste-bg" />
+    );
+
+    expect(container.querySelector(`span`)).toHaveStyle({
+      backgroundImage: `url('/teste-bg')`,
+      backgroundPosition: 'center center'
+    });
+  });
+});
