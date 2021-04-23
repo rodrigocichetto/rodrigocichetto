b1c50c4ee99a8dfea96205451af03d91a328bef8
rodrigocichetto <rodrigocichetto@gmail.com>
2021-04-23 08:50:03 -0300

    feat: add Tag component

---
+++ src/components/Tag/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react';
+
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Tag from '.';
+
+describe('<Tag />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Tag>body</Tag>);
+
+    expect(screen.getByText('<body>')).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render close tag', () => {
+    renderWithTheme(<Tag>/body</Tag>);
+
+    expect(screen.getByText('</body>')).toBeInTheDocument();
+  });
+
+  it('should render auto-close tag', () => {
+    renderWithTheme(<Tag>img /</Tag>);
+
+    expect(screen.getByText('<img />')).toBeInTheDocument();
+  });
+});
