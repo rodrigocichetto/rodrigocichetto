bd5047586b7e8f0e3a5983d8b749ca1bf7680308
rodrigocichetto <rodrigocichetto@gmail.com>
2021-05-21 14:03:33 -0300

    feat: add Profile component

--- src/components/Tag/stories.tsx
+++ src/components/Tag/stories.tsx
@@ -1,24 +1,25 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
-import Tag from '.';
+
+import Tag, { TagProps } from '.';
 
 export default {
   title: 'Tag',
   component: Tag
 } as Meta;
 
-export const Default: Story = args => <Tag {...args} />;
+export const Default: Story<TagProps> = args => <Tag {...args} />;
 
 Default.args = {
   children: 'body'
 };
 
-export const Close: Story = args => <Tag {...args} />;
+export const Close: Story<TagProps> = args => <Tag {...args} />;
 
 Close.args = {
   children: '/body'
 };
 
-export const AutoClose: Story = args => <Tag {...args} />;
+export const AutoClose: Story<TagProps> = args => <Tag {...args} />;
 
 AutoClose.args = {
   children: 'img /'
