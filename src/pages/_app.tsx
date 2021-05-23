efb41363bfcfe1d0ad51a2cd7c2980316891086f
rodrigocichetto <rodrigocichetto@gmail.com>
2021-05-23 07:59:23 -0300

    feat: add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 import GlobalStyles from 'styles/global';
 
@@ -11,14 +12,33 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Head>
-        <title>Rodrigo Cichetto // &#x1F3AE; shibetto</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content={theme.colors.background} />
-        <meta
-          name="description"
-          content="Desenvolvedor Front-end &#x1F468;&#x1F4BB; e Gamer, sou amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Rodrigo Cichetto // &#x1F3AE; shibetto"
+
+        <NextSeo
+          title="Rodrigo Cichetto // &#x1F3AE; shibetto"
+          description="Desenvolvedor Front-end &#x1F468;&#x1F4BB; formado em Ciência da Computação, Gamer &#x1F3AE;, amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Este sou eu, Rodrigo Cichetto"
+          canonical="https://cichetto.com.br/"
+          openGraph={{
+            url: 'https://cichetto.com.br/',
+            title: 'Rodrigo Cichetto',
+            description:
+              'Desenvolvedor Front-end &#x1F468;&#x1F4BB; formado em Ciência da Computação, Gamer &#x1F3AE;, amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Este sou eu, Rodrigo Cichetto',
+            images: [
+              {
+                url:
+                  'https://rodrigocichetto.s3.us-east-2.amazonaws.com/social-share.jpg'
+              }
+            ],
+            locale: 'pt_BR'
+          }}
+          twitter={{
+            handle: '@rodrigocichetto',
+            site: '@site',
+            cardType: 'summary_large_image'
+          }}
         />
       </Head>
       <GlobalStyles />
