2b42704e6cb3b911dde4fa71e5a622729f9020d4
rodrigocichetto <rodrigocichetto@gmail.com>
2021-05-23 08:51:05 -0300

    fix: adjustments

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withPWA({
   pwa: {
@@ -10,4 +10,4 @@ module.exports = withPWA({
   future: {
     webpack5: true
   }
-})
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,32 +16,34 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content={theme.colors.background} />
-
-        <NextSeo
-          title="Rodrigo Cichetto // &#x1F3AE; shibetto"
-          description="Desenvolvedor Front-end &#x1F468;&#x1F4BB; formado em Ciência da Computação, Gamer &#x1F3AE;, amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Este sou eu, Rodrigo Cichetto"
-          canonical="https://cichetto.com.br/"
-          openGraph={{
-            url: 'https://cichetto.com.br/',
-            title: 'Rodrigo Cichetto',
-            description:
-              'Desenvolvedor Front-end &#x1F468;&#x1F4BB; formado em Ciência da Computação, Gamer &#x1F3AE;, amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Este sou eu, Rodrigo Cichetto',
-            images: [
-              {
-                url:
-                  'https://rodrigocichetto.s3.us-east-2.amazonaws.com/social-share.jpg'
-              }
-            ],
-            locale: 'pt_BR'
-          }}
-          twitter={{
-            handle: '@rodrigocichetto',
-            site: '@site',
-            cardType: 'summary_large_image'
-          }}
-        />
       </Head>
+
+      <NextSeo
+        title="Rodrigo Cichetto // &#x1F3AE; shibetto"
+        description="Desenvolvedor Front-end &#x1F468;&#x1F4BB; formado em Ciência da Computação, Gamer &#x1F3AE;, amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Este sou eu, Rodrigo Cichetto"
+        canonical="https://cichetto.com.br/"
+        openGraph={{
+          url: 'https://cichetto.com.br/',
+          title: 'Rodrigo Cichetto',
+          description:
+            'Desenvolvedor Front-end &#x1F468;&#x1F4BB; formado em Ciência da Computação, Gamer &#x1F3AE;, amante do mundo do design e apaixonado pelas melhores tecnologias de desenvolvimento. Este sou eu, Rodrigo Cichetto',
+          images: [
+            {
+              url:
+                'https://rodrigocichetto.s3.us-east-2.amazonaws.com/social-share.jpg'
+            }
+          ],
+          locale: 'pt_BR'
+        }}
+        twitter={{
+          handle: '@rodrigocichetto',
+          site: '@site',
+          cardType: 'summary_large_image'
+        }}
+      />
+
       <GlobalStyles />
+
       <Component {...pageProps} />
     </ThemeProvider>
   );
